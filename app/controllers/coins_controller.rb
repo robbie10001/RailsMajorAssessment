49984a70d7ed20371582c71b771633c4a41cfb48
robbie10001/rn <robbie_c1000@hotmail.com>
2019-10-28 10:15:52 +1100

    first assessment upload

---
+++ app/controllers/coins_controller.rb
@@ -0,0 +1,35 @@
+class CoinsController < ApplicationController 
+
+    def index 
+
+    end 
+    
+    def predecimal
+    
+    end 
+    
+    def postdecimal
+    
+    end 
+
+    def forsale
+
+    end
+
+
+    def listings 
+
+
+    end 
+
+
+    def signin 
+
+
+    end 
+    
+
+
+
+    
+end 
---
+++ config/routes.rb
@@ -0,0 +1,29 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get "/", to: "coins#index", as: "root"
+get "/predecimal", to: "coins#predecimal", as: "predecimal"
+get "/postdecimal", to: "coins#postdecimal", as: "postdecimal"
+get "/forsale", to: "coins#forsale", as: "forsale"
+post "/forsale", to: "coins#forsale", as: "listcoin"
+patch "/forsale", to: "coins#forsale", as: "updatecoin"
+delete "/forsale", to: "coins#forsale", as: "deletecoin"
+get "/listing", to: "coins#listing", as: "listing"
+get "/signin", to: "coins#signin", as: "signin"
+post "/signin", to: "coins#signin", as: "updateprofile"
+delete "/sigin", to: "coins#signin", as: "deleteprofile"
+post "/signin", to: "coins#signin", as: "createprofile"
+
+
+end 
+
+
+=begin
+post "/coins", to: "coins#create"
+get "/coins/new", to: "coins#new", as: "new_coin"
+get "/coins/:id/edit", to: "coins#edit", as: "edit_coin"
+get "/coins/:id", to: "coins#show", as: "coin"
+patch "/coins/:id", to: "coins#update"
+put "/coins/:id", to: "coins#update"
+delete "/coins/:id", to: "coins#destroy"
+end
+=end
