a0a92e5c16f1f73068335f7125a3c7518b3b44a7
robbie10001/rn <robbie_c1000@hotmail.com>
2019-10-31 15:25:08 +1100

    search bar implemented

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -16,4 +16,12 @@ class ListingsController < ApplicationController
 
     end 
 
+    def search 
+        if Coin.all.pluck(:denomination).include?(params[:search]) == true 
+            redirect_to(coindenomination_path(params[:search]))
+        else 
+            redirect_back(fallback_location: buycoins_path, alert: "Please make sure to search with some data") 
+        end
+
+    end 
 end 
